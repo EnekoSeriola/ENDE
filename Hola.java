e3e44ceb02e076d17b8cd7dd3be445697a8724d4
Eneko <eneko.seriola@gmail.com>
2018-02-26 14:03:47 +0100

    dfgfd

---
+++ Hola.java
@@ -0,0 +1,28 @@
+
+/**
+ *  class Hola  
+ * 
+ * @author  
+ * @version  
+ */
+public class Hola
+{
+     
+    private int x;
+
+    /**
+     * Constructor de la clase Hola
+     */
+    public Hola()
+    {
+     
+    }
+
+    /**
+     *  
+     */
+    public void metodo()
+    {
+         
+    }
+}
